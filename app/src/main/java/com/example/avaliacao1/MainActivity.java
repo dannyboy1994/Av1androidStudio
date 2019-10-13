41a81c3f86cd72ab2d67bf319059eb8f12c7a047
Daniel Rodrigues <daniel_rodrigueess@yahoo.com.br>
2019-10-13 15:17:25 -0400

    Start commit! Add Activities

--- app/src/main/java/com/example/avaliacao1/MainActivity.java
+++ app/src/main/java/com/example/avaliacao1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.avaliacao1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void acessarSetor (View v) {
+
+    }
+
+    public void cadastrarItem (View v) {
+        AlertDialog Alertdialog;
+        Alertdialog = new AlertDialog.Builder(this).create();
+        Alertdialog.setTitle("Teste");
+        Alertdialog.setMessage("Funfou");
+        Alertdialog.show();
     }
 }
---
+++ app/src/main/java/com/example/avaliacao1/cadastroItens.java
@@ -0,0 +1,14 @@
+package com.example.avaliacao1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cadastroItens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_itens);
+    }
+}
