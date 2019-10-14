854b2144a46807b021e40fba7278d3cd17b16358
Daniel Rodrigues <daniel_rodrigueess@yahoo.com.br>
2019-10-14 09:10:40 -0400

    Criando Base de dados

--- app/src/main/java/com/example/avaliacao1/MainActivity.java
+++ app/src/main/java/com/example/avaliacao1/MainActivity.java
@@ -3,19 +3,30 @@ package com.example.avaliacao1;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Button cadastro;
     Button acessoSetor;
 
+    private SQLiteDatabase codigos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Criação da tabela para armazenar os dados dos itens
+        codigos = this.openOrCreateDatabase("codigos", Context.MODE_PRIVATE, null);
+        codigos.execSQL("CREATE TABLE if not exists objetos (id_item integer primary key autoincrement, item varchar(50), code INTEGER, setor varchar(30))");
 
         acessoSetor = (Button) findViewById(R.id.acessarSetor);
         acessoSetor.setOnClickListener(new View.OnClickListener() {
@@ -41,5 +52,17 @@ public class MainActivity extends AppCompatActivity {
     public void acessarSetor() {
         Intent intent = new Intent(this, lerCodigos.class);
         startActivity(intent);
+    }
+
+    public void carregarTabela() {
+        Cursor c = codigos.rawQuery("SELECT id_item, item, code, setor FROM objetos;", new String[]{});
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.resultado);
+        linearLayout.removeAllViews();
+        while (c.moveToNext()) {
+            TextView textView = new TextView(this);
+            textView.setText(c.getString(1) + ", " + c.getString(2) + "," + c.getString(3));
+            linearLayout.addView(textView);
+        }
     }
 }
--- app/src/main/java/com/example/avaliacao1/lerCodigos.java
+++ app/src/main/java/com/example/avaliacao1/lerCodigos.java
@@ -2,14 +2,35 @@ package com.example.avaliacao1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class lerCodigos extends AppCompatActivity {
+
+    Button listar;
+    Button lerCodigo;
+    Button voltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ler_codigos);
+
+        voltar = (Button) findViewById(R.id.voltar);
+        voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                voltarInicio();
+            }
+        });
     }
+
+    public void voltarInicio() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+
 }
